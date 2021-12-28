cfe5c691aabcfec81706aadb9f190bfc1360ace1
Noyonika Gupta <71960903+ngg17@users.noreply.github.com>
2021-12-28 16:52:06 +0530

    Add files via upload

--- throw.js
+++ throw.js
@@ -20,14 +20,37 @@ AFRAME.registerComponent("bowling-balls", {
           y: pos.y-1.2,
           z: pos.z,
         });
+
         var camera = document.querySelector("#camera").object3D;
         var direction = new THREE.Vector3();
         camera.getWorldDirection(direction);
         ball.setAttribute("velocity", direction.multiplyScalar(-10));
         var scene = document.querySelector("#scene");
+        ball.setAttribute("dynamic-body", {
+          shape: "sphere",
+          mass: "10",
+        });
+        ball.addEventListener("collide", this.removeBall);
+
         scene.appendChild(ball);
       }
     });
+  },
+  removeBall: function (e) {
+    var element = e.detail.target.el;
+    var elementHit = e.detail.body.el;
+
+    if (elementHit.id.includes("pin")) {
+      var impulse = new CANNON.Vec3(0,1,-15);
+      var worldPoint = new CANNON.Vec3().copy(
+        elementHit.getAttribute("position")
+      );
+
+      elementHit.body.applyForce(impulse, worldPoint);
+      element.removeEventListener("collide", this.removeBall);
+      var scene = document.querySelector("#scene");
+      scene.removeChild(element);
+    }
   },
 });
 
